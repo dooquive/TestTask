ca1b1a928b8c9d7aa2b8399f66151696e34e5cda
dooquive <sb2009@yandex.ru>
2019-01-21 21:17:55 +0300

    Test project

---
+++ TestTask/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace TestTask
+{
+    partial class TestTask
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvSourceTable = new System.Windows.Forms.DataGridView();
+            this.User_Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Country = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvResultTable = new System.Windows.Forms.DataGridView();
+            this.RCountry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SumCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumUniqIds = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(595, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.parseToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // parseToolStripMenuItem
+            // 
+            this.parseToolStripMenuItem.Name = "parseToolStripMenuItem";
+            this.parseToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.parseToolStripMenuItem.Text = "Parse";
+            this.parseToolStripMenuItem.Click += new System.EventHandler(this.parseToolStripMenuItem_Click);
+            // 
+            // dgvSourceTable
+            // 
+            this.dgvSourceTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSourceTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.User_Id,
+            this.Count,
+            this.Country});
+            this.dgvSourceTable.Location = new System.Drawing.Point(12, 41);
+            this.dgvSourceTable.Name = "dgvSourceTable";
+            this.dgvSourceTable.Size = new System.Drawing.Size(343, 150);
+            this.dgvSourceTable.TabIndex = 1;
+            // 
+            // User_Id
+            // 
+            this.User_Id.HeaderText = "User Id";
+            this.User_Id.Name = "User_Id";
+            this.User_Id.ReadOnly = true;
+            // 
+            // Count
+            // 
+            this.Count.HeaderText = "Count";
+            this.Count.Name = "Count";
+            this.Count.ReadOnly = true;
+            // 
+            // Country
+            // 
+            this.Country.HeaderText = "Country";
+            this.Country.Name = "Country";
+            this.Country.ReadOnly = true;
+            // 
+            // dgvResultTable
+            // 
+            this.dgvResultTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RCountry,
+            this.SumCount,
+            this.NumUniqIds});
+            this.dgvResultTable.Location = new System.Drawing.Point(12, 239);
+            this.dgvResultTable.Name = "dgvResultTable";
+            this.dgvResultTable.Size = new System.Drawing.Size(343, 150);
+            this.dgvResultTable.TabIndex = 2;
+            // 
+            // RCountry
+            // 
+            this.RCountry.HeaderText = "Country";
+            this.RCountry.Name = "RCountry";
+            this.RCountry.ReadOnly = true;
+            // 
+            // SumCount
+            // 
+            this.SumCount.HeaderText = "Sum (Count)";
+            this.SumCount.Name = "SumCount";
+            this.SumCount.ReadOnly = true;
+            // 
+            // NumUniqIds
+            // 
+            this.NumUniqIds.HeaderText = "NumUniqIds";
+            this.NumUniqIds.Name = "NumUniqIds";
+            this.NumUniqIds.ReadOnly = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(437, 338);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(82, 51);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // TestTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 443);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvResultTable);
+            this.Controls.Add(this.dgvSourceTable);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TestTask";
+            this.Text = "Test task";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parseToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvSourceTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn User_Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Country;
+        private System.Windows.Forms.DataGridView dgvResultTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RCountry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SumCount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumUniqIds;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ TestTask/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace TestTask
+{
+    public partial class TestTask : Form
+    {
+        //Диалог открытия файла
+        OpenFileDialog LoadDlg;
+
+        // Массив id пользователей
+        string[] UsersIds;
+        // Массив count
+        int[] UsersCount;
+        // Массив стран
+        string[] UsersCountries;
+        // Общее количество строк в считываемом файле
+        int NumLines;
+
+        // Индекс записи в массиве
+        int Ind;
+
+        public TestTask()
+        {
+            InitializeComponent();
+
+            // Диалог открытия файла
+            LoadDlg = new OpenFileDialog();
+            // Установка начальных значений и фильтров
+            string p = Path.GetDirectoryName(Application.ExecutablePath).Replace("\\TestTask\\bin\\Debug", string.Empty);
+            string CombinedPath = p + "\\Files";
+            LoadDlg.InitialDirectory = CombinedPath;
+            LoadDlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            LoadDlg.FilterIndex = 2;
+            LoadDlg.RestoreDirectory = true;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Имя файла
+            string FName = null;
+            if (LoadDlg.ShowDialog() == DialogResult.OK)
+            {
+                FName = LoadDlg.FileName;
+                // Получение пути к файлу по имени
+                string FPath = Path.GetFullPath(FName);
+                // Поток для чтения из файла
+                FileStream f = new FileStream(FPath, FileMode.Open, FileAccess.Read);
+        
+                NumLines = File.ReadAllLines(FPath).Length;
+                // Выделение памяти под массивы
+                UsersIds       = new string[NumLines];
+                UsersCount     = new int[NumLines];
+                UsersCountries = new string[NumLines];
+
+                // Текущая считываемая строка
+                string CurrentStr;
+                // Массив подстрок CurrentStr
+                string[] Str;
+                // Вспомогательная переменная для проверки формата поля count
+                int Num;
+                Ind = 0;
+
+                // Чтение данных из потока
+                StreamReader rd = new StreamReader(f);
+
+                while (rd.EndOfStream != true)
+                {
+                    CurrentStr = rd.ReadLine();
+                    Str = CurrentStr.Split(';');
+
+                    if (Str.Length == 3)
+                    {
+                        if (int.TryParse(Str[1], out Num) && Num >=0)
+                        {
+                            if (Str[2].All(ch => Char.IsLetter(ch)))
+                            {
+                                UsersIds[Ind]       = Str[0];
+                                UsersCount[Ind]     = Num;
+                                UsersCountries[Ind] = Str[2];
+
+                                // Добавление считанной строки в таблицу (объект DataGridView)
+                                dgvSourceTable.Rows.Add(UsersIds[Ind], UsersCount[Ind], UsersCountries[Ind]);
+
+                                Ind++;
+                            } else { continue; }
+                        } else { continue; }
+                    } else { continue; }
+                }
+
+                rd.Close();
+            }
+        }
+
+        private void parseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int SumCount;
+            int NumUniqIds;
+
+            for (int i = 0; i < NumLines; i++)
+            {
+                if(UsersCountries[i] != null)
+                {
+                    // Сумма по count для фиксированной страны
+                    SumCount = UsersCount[i];
+                    // Количество уникальных id
+                    NumUniqIds = 1;
+
+                    for (int j = i + 1; j < NumLines; j++)
+                    {
+                        // Проверка совпадения стран (без учёта регистра)
+                        if (string.Compare(UsersCountries[i], UsersCountries[j], true) == 0) 
+                        {
+                            // Подчёт суммы по count
+                            SumCount += UsersCount[j];
+                            // Если id совпадают, то количетство уникальных не увеличивается
+                            NumUniqIds = UsersIds[i] == UsersIds[j] ? NumUniqIds : NumUniqIds+1;
+                            // "Обнуление" одинаковых стран в общем массиве
+                            UsersCountries[j] = null;
+                        }
+                    }
+                } else { continue; }
+
+                // Добавление строки в итоговую таблицу (объект DataGridView)
+                dgvResultTable.Rows.Add(UsersCountries[i], SumCount, NumUniqIds);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Очистка таблиц
+            dgvSourceTable.Rows.Clear();
+            dgvResultTable.Rows.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
